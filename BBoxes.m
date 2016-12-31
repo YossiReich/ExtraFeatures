6bebed330ea746fadd25bc14ba045cb97a8c7977
Yosi Reich <yreich@apple.com>
2016-12-31 19:58:22 +0200

    BBoxes, FrameData - create classes

---
+++ BBoxes.m
@@ -0,0 +1,15 @@
+classdef BBoxes
+    % This class holds the bounding boxes that are gathered from the V&J
+    % EyeTracker. The data is public to set and read
+    
+    properties
+        bboxEyes
+        bboxLeft
+        bboxRight
+    end
+    
+    methods
+    end
+    
+end
+
---
+++ FrameData.m
@@ -0,0 +1,13 @@
+classdef FrameData
+    % This class holds all the frame data.
+    % The data is public to set and read
+    properties
+        bboxes
+        rsPoints
+    end
+    
+    methods
+    end
+    
+end
+
